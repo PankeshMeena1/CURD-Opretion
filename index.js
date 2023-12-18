37e3956b9d1c4289b7901851f8b98fbd871ea4c5
Pankesh Meena <112115701+PankeshMeena1@users.noreply.github.com>
2023-12-18 18:04:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require ("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const multer = require ("multer");
-const path  = require("path");
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/Studentdata").then()
@@ -24,22 +22,6 @@ app.get("/stuedit/:id", stualldata.stuEdit);
 
 app.post("/editsave/:id", stualldata.stuEditsave);
 
-const storage = multer.diskStorage({
-    destination:(req, file, cb)=>{
-       cb(null, "public/images")
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.filename+"_"+Date.now()+path.extname(file.originalname))
-    }
-});
-
-const upload =multer({
-     storage: storage
-})
- 
-app.post("/stuupload", upload.single("file"),(req, res)=>{
-       console.log(req.file);  
-});
 
 app.listen(8000, (req, res)=>{
     console.log("your server is runneble on 8000 !!")
